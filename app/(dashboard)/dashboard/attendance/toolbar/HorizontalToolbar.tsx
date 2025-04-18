2e6e76d8c3d402411dc40a001c56450b2c4b4d86
caturhidayat <caturhidayat00@gmail.com>
2025-04-18 16:23:57 +0700

    refactor(app): H toolbar refine toolbar props
    WIP

--- app/(dashboard)/dashboard/attendance/toolbar/HorizontalToolbar.tsx
+++ app/(dashboard)/dashboard/attendance/toolbar/HorizontalToolbar.tsx
@@ -357,7 +357,7 @@ export default function HorizontalToolbar({
           <Button
             onClick={handleViewData}
             disabled={loading}
-            className="bg-teal-600 hover:bg-teal-700"
+            // className="bg-teal-600 hover:bg-teal-700"
           >
             {loading ? (
               "Loading..."
@@ -369,9 +369,9 @@ export default function HorizontalToolbar({
             )}
           </Button>
           <Button
-            variant={"outline"}
+            variant={"destructive"}
             onClick={handleReset}
-            className="bg-orange-400 hover:bg-orange-600"
+            // className="bg-orange-400 hover:bg-orange-600"
           >
             <>
               <RotateCcw className="h-4 w-4 mr-2" />
